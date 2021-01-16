d4fdbbda5ab09f3f91266038bb6b62a99a41717a
Tyler Meador <tmeador13@gmail.com>
2021-01-15 22:05:26 -0600

    fetch from server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Form, Row, Input, Label, Col} from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 
 const required = (val) => val && val.length;
@@ -44,7 +46,7 @@ class CommentForm extends Component{
 								<Row className="form-group">
 									<Col md={12}>
 										<Label htmlFor="rating">Rating</Label>
-										<Control.select model=".rating" id="rating" name="rating" className="form-control">
+										<Control.select model=".rating" name="rating" className="form-control">
 											<option>1</option>
 											<option>2</option>
 											<option>3</option>
@@ -98,7 +100,7 @@ class CommentForm extends Component{
 			return (
 				<div className="col-12 col-md-5 m-1">
 					<Card>
-						<CardImg top src={dish.image} alt={dish.name} />
+						<CardImg top src={baseUrl + dish.image} alt={dish.name} />
 						<CardBody>
 							<CardTitle>{dish.name}</CardTitle>
 							<CardText>{dish.description}</CardText>
@@ -146,7 +148,25 @@ class CommentForm extends Component{
 		
 		console.log('DishDetail Component render invoked');
 		
-		if(props.dish != null)
+		if(props.isLoading){
+			return(
+				<div className="container">
+					<div className="row">
+						<Loading />
+					</div>
+				</div>
+			);
+		}
+		else if( props.errMess){
+			return(
+				<div className="container">
+					<div className="row">
+						<h4>{props.errMess}</h4>
+					</div>
+				</div>
+			);
+		}
+		else if(props.dish != null)
 			return(
 				<div className="container">
 				<div className="row">
